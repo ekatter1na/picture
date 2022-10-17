a6231aa865500e2b38a5bc8055cd393770d27196
ekatter1na <98614159+ekatter1na@users.noreply.github.com>
2022-10-17 20:47:55 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static final String IN_FILE_TXT = "inFile.txt";
+    private static final String OUT_FILE_TXT = "outFile.txt";
+    private static final String PICTURES_PICTURES = "pictures\\pictures";
+
+    public static void main(String[] args) {
+        String Url;
+        try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
+             BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_TXT))) {
+            while ((Url = inFile.readLine()) != null) {
+                URL url = new URL(Url);
+
+                String result;
+                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                    result = bufferedReader.lines().collect(Collectors.joining("\n"));
+                }
+                Pattern email_pattern = Pattern.compile("https:\\/\\/img3\\.fonwall\\.ru\\/o\\/(.+)\\/(.+)=350");
+                Matcher matcher = email_pattern.matcher(result);
+                while (matcher.find()) {
+                    outFile.write(matcher.group() + "\r\n"); //запись найденных ссылок
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedReader musicFile = new BufferedReader(new FileReader(OUT_FILE_TXT))) {
+            String pictures;
+            int count = 1;
+            try {
+                while ((pictures = musicFile.readLine()) != null) {
+                    downloadUsingNIO(pictures, PICTURES_PICTURES + String.valueOf(count) + ".jpg");
+                    count++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void downloadUsingNIO(String strUrl, String file) throws IOException {
+        URL url = new URL(strUrl);
+        ReadableByteChannel byteChannel = Channels.newChannel(url.openStream());
+        FileOutputStream stream = new FileOutputStream(file);
+        stream.getChannel().transferFrom(byteChannel, 0, Long.MAX_VALUE);
+        stream.close();
+        byteChannel.close();
+    }
+}
+
